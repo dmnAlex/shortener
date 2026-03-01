c00b5133afa78a8a3b2c2644f495cbed65b4f170
hsensor <dmnalex@yandex.ru>
2026-03-01 18:02:30 +0700

    добавил  возможность конфигурации приложения с помощью файла json

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,13 +1,16 @@
 package config
 
 import (
-	"errors"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"os"
 	"strconv"
 	"strings"
 
+	"dario.cat/mergo"
 	"github.com/caarlos0/env"
+	"github.com/pkg/errors"
 )
 
 const (
@@ -44,23 +47,26 @@ func (a *Address) UnmarshalText(text []byte) error {
 }
 
 type Config struct {
-	LaunchAddress   Address `env:"SERVER_ADDRESS"`
-	ShortenAddress  string  `env:"BASE_URL"`
-	LogLevel        string  `env:"LOG_LEVEL"`
-	FileStoragePath string  `env:"FILE_STORAGE_PATH"`
-	DatabaseDSN     string  `env:"DATABASE_DSN"`
-	MigrationsPath  string  `env:"MIGRATIONS_PATH"`
-	JWTSecret       string  `env:"JWT_SECRET"`
-	AuditFile       string  `env:"AUDIT_FILE"`
-	AuditURL        string  `env:"AUDIT_URL"`
-	PprofAddress    string  `env:"PPROF_ADDRESS"`
-	EnableHTTPS     bool    `env:"ENABLE_HTTPS"`
+	LaunchAddress   Address `env:"SERVER_ADDRESS" json:"server_address"`
+	ShortenAddress  string  `env:"BASE_URL" json:"base_url"`
+	LogLevel        string  `env:"LOG_LEVEL" json:"log_level"`
+	FileStoragePath string  `env:"FILE_STORAGE_PATH" json:"file_storage_path"`
+	DatabaseDSN     string  `env:"DATABASE_DSN" json:"database_dsn"`
+	MigrationsPath  string  `env:"MIGRATIONS_PATH" json:"migrations_path"`
+	JWTSecret       string  `env:"JWT_SECRET" json:"jwt_secret"`
+	AuditFile       string  `env:"AUDIT_FILE" json:"audit_file"`
+	AuditURL        string  `env:"AUDIT_URL" json:"audit_url"`
+	PprofAddress    string  `env:"PPROF_ADDRESS" json:"pprof_address"`
+	EnableHTTPS     bool    `env:"ENABLE_HTTPS" json:"enable_https"`
 }
 
 func New() (*Config, error) {
 	cfg := &Config{
 		LaunchAddress: Address{Host: defaultHost, Port: defaultPort},
 	}
+	var configPath string
+	flag.StringVar(&configPath, "c", "", "path to config file")
+	flag.StringVar(&configPath, "config", "", "path to config file")
 	flag.Var(&cfg.LaunchAddress, "a", "launch address")
 	flag.StringVar(&cfg.ShortenAddress, "b", fmt.Sprintf("http://%s:%d", defaultHost, defaultPort), "shorten address")
 	flag.StringVar(&cfg.LogLevel, "l", "info", "log level")
@@ -74,9 +80,43 @@ func New() (*Config, error) {
 	flag.BoolVar(&cfg.EnableHTTPS, "s", false, "enable https")
 	flag.Parse()
 
+	if envPath := os.Getenv("CONFIG"); envPath != "" {
+		configPath = envPath
+	}
+
+	if configPath != "" {
+		jsonCfg, err := loadJSON(configPath)
+		if err != nil {
+			return nil, errors.Wrap(err, "load json")
+		}
+
+		if err := mergo.Merge(cfg, jsonCfg); err != nil {
+			return nil, errors.Wrap(err, "merge json configs")
+		}
+	}
+
 	if err := env.Parse(cfg); err != nil {
 		return nil, err
 	}
 
+	if cfg.EnableHTTPS {
+		cfg.ShortenAddress = strings.Replace(cfg.ShortenAddress, "http://", "https://", 1)
+	}
+
 	return cfg, nil
+}
+
+func loadJSON(path string) (*Config, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, errors.Wrap(err, "open file")
+	}
+	defer f.Close()
+
+	var cfg Config
+	if err := json.NewDecoder(f).Decode(&cfg); err != nil {
+		return nil, errors.Wrap(err, "decode json")
+	}
+
+	return &cfg, nil
 }
